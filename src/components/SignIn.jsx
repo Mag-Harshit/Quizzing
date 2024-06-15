873060e8038f7567f2fe3c922ff2036ee7aff242
ninfu321 <harshit@Harshits-MacBook-Pro.local>
2024-06-15 16:53:45 -0700

    Loign again

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -2,31 +2,29 @@ import React from 'react'
 
 const SignIn = () => {
   return (
-    <div className='text-center'>
-    <h1 className="signHeading">SIGN IN</h1>
-    <form className="LoginContainer ">
-    <div className="form-floating emailEntry mx-auto">
-          <input type="email" className="form-control --bs-secondary-color" id="floatingInput" placeholder="name@example.com" size="5"/>
-          <label htmlFor="floatingInput">Email address</label>
-        </div>
-    
-    <div className="form-floating passwordEntry mx-auto">
-          <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-          <label htmlFor="floatingPassword">Password</label>
-        </div>
-        <div className="d-flex justify-content-between mx-auto signBtn">
-        <button className="btn btn-secondary w-100 py-2 mx-1 signInUp" style={{backgroundColor:"oklch(0% 0.48 279 / 45%)", color:"#c8d5f9"}} type="submit">Sign in</button>
-    
-    </div>
-    <div style={{textAlign:'center', marginTop:'20px'}}>
-    <p >OR</p>
-    </div>
-    <div className="d-flex justify-content-between mx-auto signBtn">
-        <button className="btn btn-secondary w-100 py-2 mx-1 signInUp" style={{backgroundColor:"oklch(0% 0.48 279 / 45%)", color:"#c8d5f9"}} type="submit">Sign up</button>
-    </div>
-    </form>
+<main class="text-center form-signin w-100 m-auto container" >
+  <form style={{marginTop:'10%'}}>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
 
+    <div class="form-floating mx-auto w-50">
+      <input type="email" class="form-control w-100" id="floatingInput" placeholder="name@example.com"/>
+      <label for="floatingInput">Email address</label>
     </div>
+    <div class="form-floating mx-auto w-50">
+      <input type="password" class="form-control w-100" id="floatingPasswordss" placeholder="Password"/>
+      <label for="floatingPasswordss">Password</label>
+    </div>
+
+    <div class="form-check text-start my-3">
+    </div>
+    <button class="btn btn-primary w-25 py-2" type="submit">Sign in</button>
+    <p class="mt-2 mb-3 text-body-secondary">OR</p>
+    <button class="btn btn-secondary w-25 py-2 border" type="submit">Sign up</button>
+  </form>
+</main>
+
+    
+
   )
 }
 
